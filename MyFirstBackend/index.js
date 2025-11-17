9a52254a8b6cdd7f53c4f4e894d3923c92ef6689
Jaspreet Kaur <243425428+im-jaspreet@users.noreply.github.com>
2025-11-17 23:56:37 +0530

    modified index.js to send json response

--- MyFirstBackend/index.js
+++ MyFirstBackend/index.js
@@ -1,12 +1,48 @@
 require('dotenv').config()
 const express = require('express')
+
 const app = express()
-
-
 console.log("My First Backend w Chai aur Code!");
 
 // const port = 3000  // virtual port where server will run and listen for incoming requests
 const port = process.env.PORT || 3000  // to use port from .env file or default to 3000
+
+// from "api.github.com/users/im-jaspreet":
+const apiData = {
+  "login": "im-jaspreet",
+  "id": 243425428,
+  "node_id": "U_kgDODoJglA",
+  "avatar_url": "https://avatars.githubusercontent.com/u/243425428?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/im-jaspreet",
+  "html_url": "https://github.com/im-jaspreet",
+  "followers_url": "https://api.github.com/users/im-jaspreet/followers",
+  "following_url": "https://api.github.com/users/im-jaspreet/following{/other_user}",
+  "gists_url": "https://api.github.com/users/im-jaspreet/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/im-jaspreet/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/im-jaspreet/subscriptions",
+  "organizations_url": "https://api.github.com/users/im-jaspreet/orgs",
+  "repos_url": "https://api.github.com/users/im-jaspreet/repos",
+  "events_url": "https://api.github.com/users/im-jaspreet/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/im-jaspreet/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 1,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2025-11-11T20:00:45Z",
+  "updated_at": "2025-11-17T16:30:03Z"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,6 +51,12 @@ app.get('/', (req, res) => {
 app.get('/reddit', (req, res) => {
   res.send('Welcome to Reddit')
 })
+
+app.get('/github-profile', (req, res) => {
+  res.json(apiData)  // send JSON response
+  //res.send(apiData)  // this would also work, but res.json() is more appropriate for JSON data
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
